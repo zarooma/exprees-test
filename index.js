d9206c2f3c7309034c98d8ecd167be84ca15158f
zarooma <zarooma@protonmail.com>
2022-05-10 14:08:00 +0000

    wee

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("have a good day have a good day")
+const input = prompt("")
+input
+if (input == "we") {
+    console.log(`${input}eeeeeeeeeeeeeeeeeee`)
+} else {
+    console.log("no fun")
+}
